f05cb5d6bfae357c4e9fec80a42e2ea4907b0bc2
rahuldas1108 <42487507+rahuldas1108@users.noreply.github.com>
2019-10-25 17:40:56 +0530

    Remove

--- src/main/java/com/example/demoocp/gitocp/controller/OcpController.java
+++ src/main/java/com/example/demoocp/gitocp/controller/OcpController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.demoocp.gitocp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,22 +15,3 @@ public class OcpController {
 	}
 
 }
-=======
-package com.example.demoocp.gitocp.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/demo")
-public class OcpController {
-	
-	@GetMapping("/welcome")
-	public String welcome()
-	{
-		return "Welcome to openshift";
-	}
-
-}
->>>>>>> 395b43035ccaa7a815dea73eb20835a119b1acf3
